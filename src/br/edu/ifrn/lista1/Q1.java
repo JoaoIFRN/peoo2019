10e05cd86e0c646483956fc61f3cc753b3aea0bc
1724026 <1724026@PAR391876.ifrn.local>
2019-02-28 16:17:25 -0300

    Início da resolução de lista 1 em sala de aula

---
+++ src/br/edu/ifrn/lista1/Q1.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifrn.lista1;
+
+/**
+ *
+ * @author 1724026
+ */
+public class Q1 {
+    public static void main(String[] args) {
+        System.out.println("Alô, mundo.");
+    }
+}
---
+++ src/br/edu/ifrn/lista1/Q2.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifrn.lista1;
+
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author 1724026
+ */
+public class Q2 {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Número: ");
+        int numero = scanner.nextInt();
+        System.out.println("Número digitado: " + numero);
+        System.out.printf("Número digitado: %d", numero);
+        
+        String valor = 
+        JOptionPane.showInputDialog("Número: ");
+        int valorInteiro = Integer.parseInt(valor);
+        JOptionPane.showMessageDialog(null,
+                valorInteiro);
+        
+    }
+    
+}
